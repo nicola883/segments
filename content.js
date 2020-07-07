12f8e2a76bd8bedc0cc56efd421b6b69bb585bec
Nicola Raffaele Di Matteo <nicoladimatteo@sunreport.it>
2020-07-07 00:56:22 -0300

    save and draw blocks

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 var active = true;
 var id = '33eae942-8f16-4ec3-a98b-0b6409e74095';
+// the webpage to analize
+var url = window.location.toString().split('#')[0];
 
-
-var onlyRolesInGroundTruth = true;
 
 var windowWidth = window.innerWidth;
 
@@ -13,6 +13,7 @@ var firstDOM = document.body.cloneNode(true);
 var drawOn = true;
 var start = {};
 var end = {};
+var inp;
 
 var div = document.createElement("div");
 //div.setAttribute("id", id);
@@ -38,7 +39,7 @@ window.addEventListener('mouseup', function(e) {
 		end.x = e.clientX;
 		end.y = e.clientY;
 
-		var inp = document.createElement('select');
+		inp = document.createElement('select');
 		inp.setAttribute('id', 'role');
 		let o = document.createElement('option');
 		o.setAttribute('value', 'main');
@@ -80,19 +81,78 @@ window.addEventListener('mousemove', function(e) {
 
 
 function saveDiv() {
-	/*
-	chrome.runtime.sendMessage(
-		{
-			action: 'xhttp',
-			method: 'POST', 
-			data: myBody,
-			url: 'http://localhost:8081'
-		}, function(segments) {
-	*/
+		// Create a record for the evaluation
+		// Send to the server the coordinates of the blocks
+		var pUrl = 'http://localhost/helloroles/server/resource/evaluations?single&key=df9795cc-d73a-48d1-82b9-e9e5b0b72d8a';
+		var gUrl = 'http://localhost/helloroles/server/resource/evaluations?key=df9795cc-d73a-48d1-82b9-e9e5b0b72d8a';
+		var send = {};
+		send.url = url;
+		send.role = inp.value;
+		send.tool = 'segments';
+		send.block = '(' + start.x + ',' + start.y + '),' + ' (' + end.x + ',' + end.y + ')';
+		console.log(start, end, send.block);
+		chrome.runtime.sendMessage(
+			{
+				action: 'xhttp',
+				method: 'POST',
+				data: send,
+				url: pUrl
+			}, function(page) {
+				chrome.runtime.sendMessage(
+					{
+						action: 'xhttp',
+						method: 'GET',
+						url: gUrl + '&tool=segments&url=' + url
+ 					}, function(list) {
+						drawBlocks(list);
+					}
+				);
+			}
+		);
+}
+
+function drawBlocks(list) {
+	//document.body = firstDOM.cloneNode(true);
+	for (let i=0; i<list.length; i++) {
+		let co = list[i].block.replace(/[\(\)]/g, '').split(',');
+		let d = document.createElement("div");
+		document.body.appendChild(d);
+		let h = co[1] - co[3];
+		let w = co[0] - co[2];
+		let a = 'position:absolute; top:' + co[3] +'px;left: ' + co[2] + 'px;background-color:red; height: '+ h +'px; width: ' + w + 'px;';
+		console.log(a);
+		d.setAttribute("style", a);
+	}
 }
 
 function deleteDiv() {
-	console.log('remove');
+		// Create a record for the evaluation
+		var gtUrl = 'http://localhost/helloroles/server/resource/evaluations?single&key=df9795cc-d73a-48d1-82b9-e9e5b0b72d8a&delete&url=' + url;
+		chrome.runtime.sendMessage (
+			{
+				action: 'xhttp',
+				method: 'POST',
+				url: gtUrl
+			}, function(page) {
+					// Send to the server the coordinates of the blocks
+					var tUrl = 'http://localhost/helloroles/server/resource/evaluations?single&key=df9795cc-d73a-48d1-82b9-e9e5b0b72d8a';
+					var send = {};
+					send.url = url;
+					send.role = inp.value;
+					send.tool = 'segments';
+					send.block = '(' + end.x + ',' + end.y + '),' + ' (' + start.x + ',' + start.y + ')';
+					chrome.runtime.sendMessage(
+						{
+							action: 'xhttp',
+							method: 'POST',
+							data: send,
+							url: tUrl
+						}, function(page) {
+						
+						}
+					);
+				}			
+		);
 }
 
 /** 
